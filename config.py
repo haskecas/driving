7f3bead8922396a59338a8de34236ea791fa4da6
haskecas <167912465+haskecas@users.noreply.github.com>
2026-04-22 23:21:00 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_USERNAME = 'InstructorIFBot'
+TARGET_INSTRUCTOR_SURNAME = 'Реке'
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+SESSION_STR = os.environ.get("SESSION_STR")
+USER_ID = os.environ.get("USER_ID")
